d3b5daf3ab03ec8dc6fbe5124b56a88cc346bb0b
mugikohaone-netizen <mugikoha.one@gmail.com>
2026-06-25 20:12:28 +0900

    🔄 SW更新 vmqtekm4d

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-mqtdjo15';
+const CACHE = 'app-mqtekm4d';
 const FILES = ['./','./index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
